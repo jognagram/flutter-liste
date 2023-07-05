5b9d29599a7e19fad12ffccf89137895af2db2e4
jogna <jordan.gnamien@smile.ci>
2023-07-05 19:54:26 +0000

    Add and completed task

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,143 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Gestion de tache',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoListPage> createState() => _TodoListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoListPageState extends State<TodoListPage> {
+  //Nous avons deux listes pour stocker les taches : _todoList, _completeList
+  final List<String> _todoList = [];
+  final List<String> _completedList = [];
 
-  void _incrementCounter() {
+  //Les méthodes '_addToDoList' et _markAsCompleted' seront utilisé pour ajouter des tâches respectivements
+  void _addToTodoList(String task) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _todoList.add(task);
+    });
+  }
+
+  void _markAsCompleted(int index) {
+    setState(() {
+      _completedList.add(_todoList[index]);
+      _todoList.removeAt(index);
     });
   }
 
   @override
+  //La méthode build crée une interface utilisateur avec un 'DefaultTabController' et un 'TabBarView'. Le TabBarViex a deux onglets, l'un pour les taches en cours et l'autre pour les taches terminées
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Gestion de tache'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.list), text: 'To-Do List'),
+                Tab(icon: Icon(Icons.done), text: 'Completed Tasks'),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          body: TabBarView(
+            children: [
+              _buildTodoListTab(),
+              _buildCompletedTasksTab(),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _showAddTaskDialog(context),
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        ));
+  }
+
+  @override
+//Les methodes '_buildTodoListTab et buildCompletedTab créent des 'ListView.builder pour afficher les tâches en cours et les tâches terminées, respectivement
+  Widget _buildTodoListTab() {
+    return ListView.builder(
+      itemCount: _todoList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_todoList[index]),
+          trailing: IconButton(
+            icon: const Icon(Icons.check),
+            onPressed: () => _markAsCompleted(index),
+          ),
+        );
+      },
     );
+  }
+
+  Widget _buildCompletedTasksTab() {
+    return ListView.builder(
+      itemCount: _completedList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_completedList[index]),
+          leading: const Icon(
+            Icons.done,
+            color: Colors.green,
+          ),
+        );
+      },
+    );
+  }
+
+  //La méthode _showAddTaskDialog affiche une boite de dialog permettant d'ajouter une nouvelle tache a la liste des taches
+  void _showAddTaskDialog(BuildContext context) {
+    // Créez un TextEditingController local là méthode pour éviter les fuites de memoires
+    TextEditingController taskController = TextEditingController();
+
+    //Fonction pour nettoyer le TextEditing controler après une utilisation
+    void _clearController() {
+      taskController.dispose();
+    }
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a new task'),
+            content: TextField(
+              controller: taskController,
+              decoration: InputDecoration(hintText: 'Enter task name'),
+            ),
+            actions: [
+              TextButton(
+                child: const Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _clearController();
+                },
+              ),
+              TextButton(
+                child: const Text('Add'),
+                onPressed: () {
+                  _addToTodoList(taskController.text);
+                  Navigator.of(context).pop();
+                  _clearController();
+                },
+              ),
+            ],
+          );
+        });
   }
 }
